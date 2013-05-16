ebc6c73b0fbe4eaf34b1c2e10aa3dccb6f9bfd93
Matias Mancini <info@matiasmancini.com.ar>
2013-05-16 20:05:38 -0300

    Remove built in comment features

--- m320/content.php
+++ m320/content.php
@@ -8,7 +8,7 @@
 ?>
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    
+
 		<header class="entry-header">
 			<?php if ( is_sticky() ) : ?>
 				<hgroup>
@@ -18,22 +18,13 @@
 			<?php else : ?>
 			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'm320' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 			<?php endif; ?>
-            
+
 			<?php if ( 'post' == get_post_type() ) : ?>
 			<div class="entry-meta">
 				<?php m320_posted_on(); ?>
 			</div><!-- .entry-meta -->
 			<?php endif; ?>
 
-			<?php if ( comments_open() && ! post_password_required() ) : ?>
-            
-			<!-- @matmancini removed comment item numbers from article header
-            <div class="comments-link">
-				<?php comments_popup_link( '<span class="leave-reply">' . __( 'Reply', 'm320' ) . '</span>', _x( '1', 'comments number', 'm320' ), _x( '%', 'comments number', 'm320' ) ); ?>
-			</div>
-            -->
-            
-			<?php endif; ?>
 		</header><!-- .entry-header -->
 
 		<?php if ( is_search() ) : // Only display Excerpts for Search ?>
@@ -74,13 +65,6 @@
 			<?php endif; // End if $tags_list ?>
 			<?php endif; // End if 'post' == get_post_type() ?>
 
-			<?php if ( comments_open() ) : ?>
-			<?php if ( $show_sep ) : ?>
-			<span class="sep"> | </span>
-			<?php endif; // End if $show_sep ?>
-			<span class="comments-link"><?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'm320' ) . '</span>', __( '<b>1</b> Reply', 'm320' ), __( '<b>%</b> Replies', 'm320' ) ); ?></span>
-			<?php endif; // End if comments_open() ?>
-            
 		</footer><!-- #entry-meta -->
-        
+
 	</article><!-- #post-<?php the_ID(); ?> -->
