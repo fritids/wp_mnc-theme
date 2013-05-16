067958a7d3ef9cb603b8445ad67d8b93daeb8109
Matias Mancini <info@matiasmancini.com.ar>
2013-05-16 17:42:41 -0300

    Improve page.php

--- m320/page.php
+++ m320/page.php
@@ -8,22 +8,24 @@ get_header(); ?>
 		<div id="primary">
 			<div id="content" role="main">
 
-				<?php the_post(); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-					<header class="entry-header">
-						<h1><?php the_title(); ?></h1>
-					</header><!-- .entry-header -->
+						<header class="entry-header">
+							<h1 class="entry-title"><?php the_title(); ?></h1>
+						</header><!-- .entry-header -->
 
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'm320' ) . '</span>', 'after' => '</div>' ) ); ?>
-					</div><!-- .entry-content -->
+						<div class="entry-content">
+							<?php the_content(); ?>
+						</div><!-- .entry-content -->
 
-				</article><!-- #post-<?php the_ID(); ?> -->
+					</article><!-- #post -->
+
+				<?php endwhile; ?>
 
 			</div><!-- #content -->
 		</div><!-- #primary -->
 
+<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
