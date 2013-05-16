e2b54c353212e7586dae93be950ad4002797294c
Matias Mancini <info@matiasmancini.com.ar>
2013-05-16 17:11:47 -0300

    Clean Page title funcions

--- m320/functions.php
+++ m320/functions.php
@@ -301,28 +301,6 @@ function m320_social_buttons($networks){
 endif;
 
 /**
- * Print the <title> tag based on what is being viewed.
- */
-if ( !function_exists( 'm320_page_title' ) ) :
-function m320_page_title() {
-    global $page, $paged;
-    wp_title( '|', true, 'right' );
-
-    // Add the blog name.
-    bloginfo( 'name' );
-
-    // Add the blog description for the home/front page.
-    $site_description = get_bloginfo( 'description', 'display' );
-    if ( $site_description && ( is_home() || is_front_page() ) )
-        echo " | $site_description";
-
-    // Add a page number if necessary:
-    if ( $paged >= 2 || $page >= 2 )
-        echo ' | ' . sprintf( __( 'Page %s', 'm320' ), max( $paged, $page ) );
-}
-endif;
-
-/**
  * Prints HTML date and author for the current post.
  */
 if ( !function_exists( 'm320_posted_on' ) ) :
--- m320/header.php
+++ m320/header.php
@@ -24,7 +24,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta name="viewport" content="width=device-width" />
 
-	<title><?php m320_page_title(); ?></title>
+	<title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo('name'); ?></title>
 	<meta name="author" content="Matias Mancini" />
 
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
