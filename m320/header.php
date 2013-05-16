ff1dbecae7bc6f1ec44fa0c9fc6fc8116af59cc6
Matias Mancini <info@matiasmancini.com.ar>
2013-05-16 17:10:46 -0300

    Improve IE sniffer

--- m320/header.php
+++ m320/header.php
@@ -6,10 +6,18 @@
 * @subpackage m320
 */
 ?>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+<!--[if lt IE 8]>
+<html class="no-js ie oldie" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="no-js ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 9]>
+<html class="no-js ie ie9" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 8)| !(IE 9)  ]><!-->
+<html class="no-js" <?php language_attributes(); ?>>
+<!--<![endif]-->
 
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
