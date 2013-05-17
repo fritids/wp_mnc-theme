05498666ea9a89ef96be9747c2a4c614355b922b
Matias Mancini <info@matiasmancini.com.ar>
2013-05-17 10:15:31 -0300

    remove unused comments

--- m320/index.php
+++ m320/index.php
@@ -9,7 +9,6 @@
 get_header(); ?>
 
 		<div class="primary">
-
 			<div id="content" role="main">
 
 			<?php if ( have_posts() ) : ?>
@@ -25,19 +24,18 @@ get_header(); ?>
 				<article id="post-0" class="post no-results not-found">
 					<header class="entry-header">
 						<h1 class="entry-title"><?php _e( 'Nothing Found', 'm320' ); ?></h1>
-					</header><!-- .entry-header -->
+					</header>
 
 					<div class="entry-content">
 						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'm320' ); ?></p>
 						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
+					</div>
+				</article>
 
 			<?php endif; ?>
 
-			</div><!-- #content -->
-
-		</div><!-- #primary -->
+			</div>
+		</div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
