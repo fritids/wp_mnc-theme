4c96c8abef7d2bc0f8002cb84d338dc1cbb87adf
Matias Mancini <info@matiasmancini.com.ar>
2013-05-20 08:42:44 -0300

    Reindent JS files

--- mnc/js/script.js
+++ mnc/js/script.js
@@ -1,35 +1,35 @@
 (function (window, $, undefined) {
 
-    'use strict';
-    console.log( MNC );
+	'use strict';
+	console.log( MNC );
 
-    $(window).load(function(){
+	$(window).load(function(){
 
-        // Polyfill for placeholder, required & autofocus
-        /*var minformUrl = MNC.templateUri + '/js/minform.js';
-        Modernizr.load({
-            test: Modernizr.input.placeholder && Modernizr.input.required && Modernizr.input.autofocus,
-            nope: minformUrl,
-            complete: function(){
-                if( typeof $.fn.minform !== 'undefined' ){
-                    $('form').minform();
-                }
-            }
-        });*/
+		// Polyfill for placeholder, required & autofocus
+		/*var minformUrl = MNC.templateUri + '/js/minform.js';
+		Modernizr.load({
+		test: Modernizr.input.placeholder && Modernizr.input.required && Modernizr.input.autofocus,
+		nope: minformUrl,
+		complete: function(){
+		if( typeof $.fn.minform !== 'undefined' ){
+		$('form').minform();
+		}
+		}
+		});*/
 
-    });
+	});
 
-    $(function(){
+	$(function () {
 
-        // Magnific popup init
-        var $links = $('a[rel="gallery"]');
-        $links.magnificPopup({
-            type: 'image',
-            gallery: {
-                enabled: $links.length > 1
-            }
-        });
+		// Magnific popup init
+		var $links = $('a[rel="gallery"]');
+		$links.magnificPopup({
+			type: 'image',
+			gallery: {
+				enabled: $links.length > 1
+			}
+		});
 
-    });
+	});
 
 }(window, jQuery));
